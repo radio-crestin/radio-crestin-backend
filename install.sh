8dd5dd246bec2f99d20d98dbda9c743e113d28f9
Iosif Nicolae <iosif@mailo.dev>
2021-05-30 14:40:58 +0300

    Update docker-compose version

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@
 # ssh-keygen -t rsa
 # Configure the created ssh public key as deploy key on Github
 # git clone git@github.com:iosifnicolae2/radio-crestin.ro.git
+# cd radio-crestin.ro/
+# ./install.sh
+
 # Installing Docker
 apt update
 apt install -y apt-transport-https ca-certificates curl software-properties-common
@@ -13,4 +16,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt update
 apt install -y docker-ce docker-compose
-systemctl status docker
+
+# After this command is executed
+# cd https-streaming
+# docker-compose up -d
