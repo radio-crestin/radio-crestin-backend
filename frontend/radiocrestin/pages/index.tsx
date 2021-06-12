0c241fc1ba8eef862224cfe2ba06bc748401bd26
Iosif Nicolae <iosif@mailo.dev>
2021-06-12 11:43:05 +0300

    Add build-cloudflare command

--- frontend/radiocrestin/pages/index.tsx
+++ frontend/radiocrestin/pages/index.tsx
@@ -13,13 +13,8 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Bine ati <span style={{"color": "#0070f3"}}>venit</span>!
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
@@ -52,18 +47,18 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      {/*<footer className={styles.footer}>*/}
+      {/*  /!*<a*!/*/}
+      {/*  /!*  href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"*!/*/}
+      {/*  /!*  target="_blank"*!/*/}
+      {/*  /!*  rel="noopener noreferrer"*!/*/}
+      {/*  /!*>*!/*/}
+      {/*  /!*  Powered by{' '}*!/*/}
+      {/*  /!*  <span className={styles.logo}>*!/*/}
+      {/*  /!*    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />*!/*/}
+      {/*  /!*  </span>*!/*/}
+      {/*  /!*</a>*!/*/}
+      {/*</footer>*/}
     </div>
   )
 }
