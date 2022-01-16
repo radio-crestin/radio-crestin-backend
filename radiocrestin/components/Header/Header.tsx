a26d612d2e0e5269a9733b0c159c64a8a3787395
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-01-16 16:22:09 +0200

    Elisei/review code.

--- radiocrestin/components/Header/Header.tsx
+++ radiocrestin/components/Header/Header.tsx
@@ -8,12 +8,12 @@ export default function Header() {
     <div className={ `${ styles.containerBackground }` }>
       <div className={ `${ styles.titleLogo }` }>
         <img src={ logoImg.src } alt={ 'logo img' }/>
-        <h1 className={ `${ styles.title }` }>Radio Crestin</h1>
+        <h1 className={ `${ styles.title }` }>Radio Creștin</h1>
       </div>
 
       <div className={`${styles.contentBottom}`}>
         <div className={`${styles.welcome}`}>
-          <h1 className={styles.goodMorning}>Buna dimineata!</h1>
+          <h1 className={styles.goodMorning}>Bună dimineața!</h1>
           <h4 className={`${styles.verseOfDay}`}>Ferice de cei ce plâng, căci ei vor fi mângâiaţi! MATEI 5:4</h4>
         </div>
         <div className={`${styles.radioPlayer}`}>
