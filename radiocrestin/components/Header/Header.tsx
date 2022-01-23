87a1c8c796e60afc49c7b66b691f32d24543f1a0
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-01-23 15:57:09 +0200

    Elisei/load headerImage with next/image.

--- radiocrestin/components/Header/Header.tsx
+++ radiocrestin/components/Header/Header.tsx
@@ -1,10 +1,19 @@
 import styles from './Header.module.scss'
 import {ReactChild, ReactFragment, ReactPortal} from 'react';
+import Image from 'next/image'
 
 export default function Header(props: { children: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined; }) {
 
   return <>
     <div className={`${styles.header}`}>
+      <Image
+        src={require('../../public/images/header-bg.jpg')}
+        className={styles.headerImage}
+        layout={'fill'}
+        objectFit={'cover'}
+        alt="Background header img."
+        priority={ true }
+      />
       {props.children}
     </div>
   </>
