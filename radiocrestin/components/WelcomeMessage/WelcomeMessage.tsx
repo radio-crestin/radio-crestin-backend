1313d0ff5dbb1e098de046b4b523ba9fb2c93893
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-01-23 14:40:56 +0200

    Elisei/Set the welcome messages based on the time of the day

--- radiocrestin/components/WelcomeMessage/WelcomeMessage.tsx
+++ radiocrestin/components/WelcomeMessage/WelcomeMessage.tsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React  from "react";
 import styles from "@/components/Header/Header.module.scss";
+
+function setWelcomeMessage() {
+  const today = new Date();
+  const curHr = today.getHours();
+
+  if (curHr < 12) {
+    return "Bună dimineața!";
+  } else if (curHr < 18) {
+    return "Bună ziua!";
+  } else {
+    return "Bună seara!";
+  }
+}
 
 export default function WelcomeMessage() {
   // TODO
-  // Set the welcome messages based on the time of the day
   // Also, we need to fetch the daily verse from an API
   return <>
-    <div className={`${styles.contentBottom}`}>
-      <div className={`${styles.welcome}`}>
-        <h1 className={styles.goodMorning}>Bună dimineața!</h1>
-        <h4 className={`${styles.verseOfDay}`}>Ferice de cei ce plâng, căci ei
-          vor fi mângâiaţi! MATEI 5:4</h4>
+    <div className={ `${ styles.contentBottom }` }>
+      <div className={ `${ styles.welcome }` }>
+        <h1 className={ styles.goodMorning }>{ setWelcomeMessage() }</h1>
+        <h4 className={ `${ styles.verseOfDay }` }>
+          Ferice de cei ce plâng, căci ei vor fi mângâiaţi! MATEI 5:4</h4>
       </div>
     </div>
   </>
