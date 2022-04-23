27b428687ef2cde4e654af2e3a5775446a411f91
Iosif Nicolae <iosif@mailo.dev>
2022-04-23 20:23:38 +0300

    Use the cdn for hls streaming

---
+++ backend/migrations/default/1650727151408_create_station_streams_functions/up.sql
@@ -0,0 +1,5 @@
+DROP FUNCTION IF EXISTS hls_stream_url(station_row stations);
+CREATE FUNCTION hls_stream_url(station_row stations)
+RETURNS TEXT AS $$
+  SELECT 'https://cdn.hls.radio-crestin.com/hls/' || station_row.slug || '/index.m3u8';
+$$ LANGUAGE sql STABLE;
