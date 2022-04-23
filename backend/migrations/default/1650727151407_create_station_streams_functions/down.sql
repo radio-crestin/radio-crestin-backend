6b941f11304c918826405309b553b369466d1c33
Iosif Nicolae <iosif@mailo.dev>
2022-04-23 18:26:14 +0300

    Add proxy & hls endpoints

---
+++ backend/migrations/default/1650727151407_create_station_streams_functions/down.sql
@@ -0,0 +1,12 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- DROP FUNCTION IF EXISTS proxy_stream_url(station_row stations);
+-- CREATE FUNCTION proxy_stream_url(station_row stations)
+-- RETURNS TEXT AS $$
+--   SELECT 'https://proxy.radio-crestin.com/' || station_row.stream_url;
+-- $$ LANGUAGE sql STABLE;
+-- DROP FUNCTION IF EXISTS hls_stream_url(station_row stations);
+-- CREATE FUNCTION hls_stream_url(station_row stations)
+-- RETURNS TEXT AS $$
+--   SELECT 'https://hls.radio-crestin.com/hls/' || station_row.slug || '/index.m3u8';
+-- $$ LANGUAGE sql STABLE;
