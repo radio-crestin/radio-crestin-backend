ea3e2e9793e5d519597e298c5b47c9b15f9631ec
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2022-10-16 22:57:11 +0300

    fix: #24 add noindex, nofollow for subdomains.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,7 @@
-import React, {useEffect} from "react";
+import React from "react";
 import { useStations } from "../hooks/stations";
-import { useLocalStorageState } from "../utils/state";
 import { getStationsMetadata } from "../backendServices/stations";
-import { Station, StationsMetadata } from "../types";
-import {useRouter} from "next/router";
+import { StationsMetadata } from "../types";
 import StationPage from "./[station_category_slug]/[station_slug]";
 
 const groupBy = function(xs: any[], key: string) {
@@ -13,26 +11,30 @@ const groupBy = function(xs: any[], key: string) {
   }, {});
 };
 
-export default function Home({stations_metadata}: {
+export default function Home({ stations_metadata, hostname }: {
   stations_metadata: StationsMetadata;
+  hostname: string;
 }) {
   const { stations, station_groups, isLoading, isError } = useStations({
     refreshInterval: 10000,
     initialStationsMetadata: stations_metadata,
   });
   return StationPage({
+    hostname,
     stations_metadata,
     station_category_slug: station_groups[0].slug,
     station_slug: stations[0].slug,
-    default_station: true
-  })
+    default_station: true,
+  });
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context: any) {
   const stations_metadata = await getStationsMetadata();
+  const { req } = context;
   return {
     props: {
       stations_metadata,
+      hostname: req.headers.host,
     },
   };
 }
