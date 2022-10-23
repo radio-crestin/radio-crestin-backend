da2b1a7c9f397ace8cf26223aba493b709182e3e
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2022-10-23 16:12:19 +0300

    #17 Hide station metadata on mobile

--- frontend/components/StationList/StationList.tsx
+++ frontend/components/StationList/StationList.tsx
@@ -1,6 +1,9 @@
-import React from "react";
-import {Station, StationGroup} from "types";
-import {CONSTANTS} from "../../lib/constants";
+import React from 'react';
+import dynamic from 'next/dynamic';
+import {isMobile} from 'react-device-detect';
+
+import {Station, StationGroup} from 'types';
+import {CONSTANTS} from '../../lib/constants';
 import {
   AspectRatio,
   Box,
@@ -8,93 +11,46 @@ import {
   Grid,
   GridItem,
   Image,
-  Text
-} from "@chakra-ui/react";
-import Link from "next/link";
-import {cdnImageLoader} from "../../utils/cdnImageLoader";
+  Text,
+} from '@chakra-ui/react';
+import Link from 'next/link';
+import {cdnImageLoader} from '../../utils/cdnImageLoader';
+
+const StationMetadata = dynamic(
+  () => import('@/components/StationMetadata/StationMetadata'),
+  {ssr: false},
+);
 
 const StationItem = (station: Station) => {
   return (
     <Box position={'relative'} role="group">
-      <AspectRatio
-        position={'relative'}
-        ratio={1}
-      >
-        <Box
-          borderRadius={{base: '20px', lg: '41px'}}>
+      <AspectRatio position={'relative'} ratio={1}>
+        <Box borderRadius={{base: '20px', lg: '41px'}}>
           <Image
             src={cdnImageLoader({
-              src: station.now_playing?.song?.thumbnail_url || station.thumbnail_url || CONSTANTS.DEFAULT_COVER,
+              src:
+                station.now_playing?.song?.thumbnail_url ||
+                station.thumbnail_url ||
+                CONSTANTS.DEFAULT_COVER,
               width: 384,
-              quality: 80
+              quality: 80,
             })}
-            alt={station.title + " - " + station.now_playing?.song?.name + " de " + station.now_playing?.song?.artist.name}
-            boxSize='100%'
-            objectFit='cover'
+            alt={`${station.title} - ${station.now_playing?.song?.name} de ${station.now_playing?.song?.artist.name}`}
+            boxSize="100%"
+            objectFit="cover"
             htmlHeight={250}
             htmlWidth={250}
             style={{
-               "filter": station?.uptime?.is_up ? "": "grayscale(1)"
+              filter: station?.uptime?.is_up ? '' : 'grayscale(1)',
             }}
           />
         </Box>
       </AspectRatio>
-      {station.now_playing?.song?.name&&<Text
-        as={'h4'}
-        position={'absolute'}
-        bottom={'110px'}
-        py={1}
-        px={1}
-        fontSize='md'
-        // mx={3}
-        color={'white'}
-        align={'left'}
-        fontWeight={'600'}
-        noOfLines={1}
-        background={'black'}
-        opacity={0}
-        transition={'opacity .2s linear'}
-        _groupHover={{ opacity: 1 }}
-      >{station.now_playing?.song?.name}</Text>}
-      {station.now_playing?.song?.artist.name && <Text
-        as={'h5'}
-        position={'absolute'}
-        bottom={'87px'}
-        py={0}
-        px={1}
-        fontSize='0.73rem'
-        // mx={3}
-        color={'white'}
-        align={'left'}
-        fontWeight={'400'}
-        noOfLines={1}
-        background={'black'}
-        opacity={0}
-        transition={'opacity .2s linear'}
-        _groupHover={{ opacity: 1 }}
-      >{station.now_playing?.song?.artist.name}</Text>}
-      {!station.now_playing?.song?.name && !station.now_playing?.song?.artist.name && <Text
-        as={'h5'}
-        position={'absolute'}
-        bottom={'87px'}
-        py={0}
-        px={1}
-        fontSize='0.73rem'
-        // mx={3}
-        color={'white'}
-        align={'left'}
-        fontWeight={'400'}
-        noOfLines={1}
-        background={'black'}
-        opacity={0}
-        transition={'opacity .2s linear'}
-        _groupHover={{ opacity: 1 }}
-      >Metadate indisponibile</Text>}
+      {!isMobile && <StationMetadata {...station} />}
       <Center mt={3}>
-        <Text fontSize='xl'
-              fontWeight='500'
-              // height={55}
-              noOfLines={1}>{station.title}</Text>
+        <Text fontSize="xl" fontWeight="500" noOfLines={1}>
+          {station.title}
+        </Text>
       </Center>
     </Box>
   );
