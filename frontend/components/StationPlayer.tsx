a9309127ad2370bd625dd181fced92f63ef79197
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2022-11-20 21:38:53 +0200

    fix: set isPlaying false on first load (because the user didn't interact with the page)

--- frontend/components/StationPlayer.tsx
+++ frontend/components/StationPlayer.tsx
@@ -19,6 +19,7 @@ import {cdnImageLoader} from '@/utils/cdnImageLoader';
 import {trackListenClientSide} from '../frontendServices/listen';
 import {CONSTANTS} from '../lib/constants';
 import {Station} from '../types';
+import {isMobile} from 'react-device-detect';
 
 const STREAM_TYPE_INFO: any = {
   HLS: {
@@ -43,6 +44,12 @@ export default function StationPlayer({stations}: any) {
   const [isPlaying, setPlaying] = useLocalStorageState(false, 'IS_PLAYING');
   const [volume, setVolume] = useLocalStorageState(60, 'AUDIO_PLAYER_VOLUME');
   const [selectedStreamType, setSelectedStreamType] = useState('HLS');
+
+  useEffect(() => {
+    if (isMobile) {
+      setPlaying(false);
+    }
+  }, []);
 
   const station: Station = stations.find(
     (station: {slug: string}) => station.slug === station_slug,
