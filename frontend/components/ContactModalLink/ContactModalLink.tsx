19b2df4fef5bc147f2283ef8355f469f8c184153
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2022-11-25 00:31:42 +0200

    fix: SEO issue (Links are not crawlable)

--- frontend/components/ContactModalLink/ContactModalLink.tsx
+++ frontend/components/ContactModalLink/ContactModalLink.tsx
@@ -1,49 +1,72 @@
 import {
+  Button,
+  Link,
   Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
   ModalBody,
-  ModalCloseButton, useDisclosure, Button,
-} from '@chakra-ui/react'
-import { Link, Text } from '@chakra-ui/react'
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
 
 export function ContactModalLink() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const {isOpen, onOpen, onClose} = useDisclosure();
   return (
     <>
-      <Link onClick={onOpen}>
+      <Button
+        onClick={onOpen}
+        isActive={true}
+        fontWeight={500}
+        padding={0}
+        variant={'link'}>
         Contact
-      </Link>
+      </Button>
 
-      <Modal isOpen={isOpen} onClose={onClose} size={'lg'} preserveScrollBarGap={true}>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        size={'lg'}
+        preserveScrollBarGap={true}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Contact</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
             <Text>
-              Dacă doriți să ne transmiteți un mesaj vă rugăm să folosiți una din modalitățile de mai jos.
+              Dacă doriți să ne transmiteți un mesaj vă rugăm să folosiți una
+              din modalitățile de mai jos.
             </Text>
             <Text mt={2.5}>
-               Email:
-              <Link href={"mailto:contact@radio-crestin.com"} color='teal.500' ml={1} isExternal>contact@radio-crestin.com</Link>
+              Email:
+              <Link
+                href={'mailto:contact@radio-crestin.com'}
+                color="teal.500"
+                ml={1}
+                isExternal>
+                contact@radio-crestin.com
+              </Link>
             </Text>
             <Text mt={0.9}>
               WhatsApp:
-              <Link href={"https://wa.me/40773994595?text=Buna%20ziua"} color='teal.500' ml={1} isExternal>+4 0773 994 595</Link>
+              <Link
+                href={'https://wa.me/40773994595?text=Buna%20ziua'}
+                color="teal.500"
+                ml={1}
+                isExternal>
+                +4 0773 994 595
+              </Link>
             </Text>
-            <br/>
+            <br />
           </ModalBody>
 
           <ModalFooter>
-            <Button onClick={onClose}>
-              Închide
-            </Button>
+            <Button onClick={onClose}>Închide</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
     </>
-  )
+  );
 }
