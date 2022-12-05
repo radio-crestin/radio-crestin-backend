71d6bbd60868c62523cdd8f4c14c4f9b31012dbb
Nicolae Iosif <iosif@bringes.io>
2022-12-05 12:09:52 +0200

    Update stations.ts

--- frontend/pages/api/v1/stations.ts
+++ frontend/pages/api/v1/stations.ts
@@ -5,7 +5,7 @@ import {StationsMetadata} from "../../../types";
 export default function handler(req: NextApiRequest, res: NextApiResponse<StationsMetadata>) {
   res.setHeader(
     'Cache-Control',
-    'public, s-maxage=10, stale-while-revalidate=59'
+    'public, s-maxage=1, stale-while-revalidate=5'
   )
   return getStationsMetadata().then(stationsMetadata => {
     res.status(200).json(stationsMetadata)
