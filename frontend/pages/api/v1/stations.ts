72299710fc1976848dfb04da6efee1a0b4679621
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2022-12-09 01:39:45 +0200

    fix: revert the Cache-Control for stations.ts

--- frontend/pages/api/v1/stations.ts
+++ frontend/pages/api/v1/stations.ts
@@ -6,6 +6,10 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<StationsMetadata>,
 ) {
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=1, stale-while-revalidate=5',
+  );
   return getStationsMetadata().then(stationsMetadata => {
     res.status(200).json(stationsMetadata);
   });
