bfbcdaa11de60670be8261b25b98d6947eacbeee
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2022-12-08 21:22:09 +0200

    fix: remove strategy worker & add GA & hotjar

--- frontend/components/Analytics/Analytics.tsx
+++ frontend/components/Analytics/Analytics.tsx
@@ -1,19 +1,36 @@
-import React from "react";
-import Script from "next/script";
+import React from 'react';
+import Script from 'next/script';
 
-export default function Analytics(props: { children?: any }) {
+export default function Analytics() {
   return (
     <>
-      {props.children}
-
-      {/*TODO: Add this after merge*/}
-      {/*<script defer src='https://static.cloudflareinsights.com/beacon.min.js'*/}
-      {/*        data-cf-beacon='{"token": "c2153a600cc94f69848e4decff1983a1"}'/>*/}
       <Script
-        strategy="worker"
-        src="https://www.googletagmanager.com/gtag/js?id=G-FDX00R8K3P"></Script>
-      <Script src="/ga.js" strategy="worker"></Script>
-      <Script src="/hj.js" strategy="worker"></Script>
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-FDX00R8K3P"
+      />
+      <Script strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag('js', new Date());
+
+          gtag('config', 'G-FDX00R8K3P');
+        `}
+      </Script>
+      <Script id="hotjar" strategy="afterInteractive">
+        {`
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:2555503,hjsv:5};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+        `}
+      </Script>
     </>
   );
 }
