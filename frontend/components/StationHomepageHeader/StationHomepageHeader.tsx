a517319df4b2b059b6d4f3e39579732124f7f1af
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2023-06-26 00:13:25 +0300

    Add buttons next/prev - Progress 1

--- frontend/components/StationHomepageHeader/StationHomepageHeader.tsx
+++ frontend/components/StationHomepageHeader/StationHomepageHeader.tsx
@@ -4,6 +4,7 @@ import StationInformation from '@/components/StationInformation/StationInformati
 import {Box, Flex} from '@chakra-ui/react';
 import SongInformation from '@/components/SongInformation/SongInformation';
 import {Station} from '../../types';
+import {ArrowBackIcon, ArrowForwardIcon} from '@chakra-ui/icons';
 
 export default function StationHomepageHeader({
   selectedStation,
@@ -15,16 +16,40 @@ export default function StationHomepageHeader({
       borderRadius={{base: 12, lg: 40}}
       mx={{base: -2, lg: 'auto'}}
       mb={{base: 0, lg: 12}}
-      px={{base: 3, lg: 16}}
+      px={{base: 3, lg: 8}}
       py={{base: 3, lg: 14}}
       bg={'brand.600'}
       flexDirection={{base: 'column-reverse', lg: 'row'}}>
+      <Box display={'flex'} alignItems={'center'}>
+        <ArrowBackIcon
+          margin={2}
+          cursor={'pointer'}
+          height="50px"
+          width="50px"
+          padding={'5px'}
+          borderRadius={30}
+          color={'black'}
+          border={'2px solid black'}
+        />
+      </Box>
       <SongInformation key={selectedStation?.id} station={selectedStation} />
       <Box
         w={{base: '100%', lg: '71%'}}
         minW={{base: 'auto', lg: '400px'}}
         pl={{base: 1, lg: 4}}>
         <StationInformation station={selectedStation} />
+      </Box>
+      <Box display={'flex'} alignItems={'center'}>
+        <ArrowForwardIcon
+          margin={2}
+          cursor={'pointer'}
+          height="50px"
+          width="50px"
+          padding={'5px'}
+          borderRadius={30}
+          color={'black'}
+          border={'2px solid black'}
+        />
       </Box>
     </Flex>
   );
