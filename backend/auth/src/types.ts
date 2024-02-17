af620377c4280ee95f68a977e9c3b49e335485d3
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2024-02-17 11:11:09 +0200

    feat: split auth as separate env

---
+++ backend/auth/src/types.ts
@@ -0,0 +1,48 @@
+export interface StationMetadataFetcherCategory {
+  slug: string;
+}
+
+export interface StationMetadataFetcher {
+  order: number;
+  url: string;
+  station_metadata_fetch_category: StationMetadataFetcherCategory;
+}
+
+export interface Song {
+  raw_title?: string | null;
+  name: string | null;
+  artist: string | null;
+  thumbnail_url: string | null;
+}
+
+export interface StationNowPlaying {
+  timestamp?: string;
+  current_song: Song | null;
+  listeners?: number | null;
+  raw_data?: any;
+  error?: any | null;
+}
+
+export interface StationUptime {
+  timestamp: string;
+  is_up: boolean;
+  latency_ms: number;
+  raw_data: any;
+}
+
+export interface Station {
+  id: number;
+  title: string;
+  stream_url: string;
+  rss_feed?: string;
+  station_metadata_fetches: StationMetadataFetcher[];
+}
+
+export interface StationRssFeed {
+  posts?: {
+    title: string;
+    link: string;
+    description: string;
+    published: string;
+  }[];
+}
